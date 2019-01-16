06da0ef0f997ff2215e194b44f0800b2d78a8d81
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2019-01-16 03:45:30 +0000

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,8 +1,8 @@
 class DecimalToBinary
 {
-public static void main(String s[])
+public static void main(String s[]) // main method jvm call always main method
 {
-int number=Integer.parseInt(s[0]);
-System.out.println(Integer.toBinaryString(number));
+int number=Integer.parseInt(s[0]); // to take input from command line
+System.out.println(Integer.toBinaryString(number)); // to.BinaryString(int number) is a method inside Integer class use to convert decimal number into binary number
 }
 }
