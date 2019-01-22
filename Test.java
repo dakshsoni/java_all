60906ad2c7c864e04fe948b8d4afec8b92358f35
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2019-01-22 06:13:24 +0000

    last 3rd repeted number occrence in array

--- Test.java
+++ Test.java
@@ -2,13 +2,24 @@ class Test
 {
 public static void main(String s[])
 {
-for(int i=0;i<5;i++)
+int a[]={1,2,3,2,4,2,5,2,60,45,2,56,2};
+int i;
+int count=0;
+for(i=12;i<=a.length;i--)
 {
-for(int j=0;j<5;j++)
+for(int j=12;j<=a.length;j--)
 {
-System.out.print(i);
+if(a[j]==(a[i]))
+{
+count++;
+if(count==3)
+{
+//return a[i];
+System.out.println(j);
 }
-System.out.println();
+}
+
+}
 }
 }
 }
