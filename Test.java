21548258eaf2224e69ee0cc3d5e28b2eef371080
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2019-02-13 12:57:55 +0530

    constructor of string class

--- Test.java
+++ Test.java
@@ -1,14 +1,35 @@
+//now i'm taking string constructor and compare these 
 class Test
+{ 
+
+void m1()
 {
-	
+char []ch={'y','a','v','a'};
+byte []b={106,98,118,97};
+String s0=new String(ch);
+System.out.println(ch);
+String s1=new String(b);
+System.out.println(b);
+for(int i=0;i<ch.length;i++)
+{
+
+System.out.println(ch[i]==b[i]);
+
+
+System.out.println(s0.equals(s1));
+
+
+System.out.println(s0.compareTo(s1));
+}
+
+}
+
+
+
 public static void main(String s[])
 {
-
-String str="dakshsoni.ds4743@gmail.com";
-
-for(int i=0;i<str.length();i++)
-{
-System.out.println(str.charAt(i));
+Test t=new Test();
+t.m1();
 }
-}
+
 }
