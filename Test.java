c2ec497bffbd0342fcfee962e2d931186bca8ef0
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2019-02-13 12:54:20 +0530

    find out char position in a string

--- Test.java
+++ Test.java
@@ -1,25 +1,14 @@
 class Test
 {
+	
 public static void main(String s[])
 {
-int a[]={1,2,3,2,4,2,5,2,60,45,2,56,2};
-int i;
-int count=0;
-for(i=12;i<=a.length;i--)
-{
-for(int j=12;j<=a.length;j--)
-{
-if(a[j]==(a[i]))
-{
-count++;
-if(count==3)
-{
-//return a[i];
-System.out.println(j);
-}
-}
 
-}
+String str="dakshsoni.ds4743@gmail.com";
+
+for(int i=0;i<str.length();i++)
+{
+System.out.println(str.charAt(i));
 }
 }
 }
