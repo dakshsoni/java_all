2e7a9abfa0004a820b439e44dbd0d68a35928ac5
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2019-02-13 12:55:13 +0530

    use of compareTo() method

---
+++ Comparetomethod.java
@@ -0,0 +1,13 @@
+//compare to method comparable interface ka hai jo value compare karega or int type ki value return karke dega
+class Comparetomethod
+{
+public static void main(String s[])
+{
+String s0="ducat";
+String s2="ducat soni 12446e5675";
+String s1=new String("ducat");
+System.out.println(s0.compareTo(s1));
+System.out.println(s0.compareTo(s2));
+}
+
+}
