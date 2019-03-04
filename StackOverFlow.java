cb9d5a082b1b110020461ca31cc8347f899b5994
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2019-03-04 13:46:47 +0530

    stack over flow condition in java

---
+++ StackOverFlow.java
@@ -0,0 +1,14 @@
+class StackOverFlow
+{
+void m1()
+{
+
+System.out.println("helo daksh");
+m1();
+}
+public static void main(String s[])
+{
+StackOverFlow t =new StackOverFlow();
+t.m1();
+}
+}
---
+++ StackOverFlowAgain.java
@@ -0,0 +1,14 @@
+class StackOverFlowAgain
+{
+StackOverFlowAgain()
+{
+
+System.out.println("helo daksh");
+new StackOverFlowAgain();
+}
+public static void main(String s[])
+{
+StackOverFlow t =new StackOverFlow();
+
+}
+}
