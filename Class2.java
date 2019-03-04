35514d50bbb06b527b42834c0ba52b21284aaf79
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2019-03-04 13:26:01 +0530

    two main method

---
+++ Class2.java
@@ -0,0 +1,18 @@
+class Class1
+{
+public static void main(String s[])
+{
+for(String s2:s)
+{
+System.out.println(s2);
+}
+}
+}
+class Class2
+{
+public static void main(String s[])
+{
+String name[]={"ravi","sonu","guddu"};
+Class1.main(name);
+}
+}
