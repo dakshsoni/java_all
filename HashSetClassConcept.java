9b4fd144278f7de9eaf80fe0f583b61f6645517b
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2019-03-09 10:02:26 +0530

    4.1. HashSet Example

---
+++ HashSetClassConcept.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+// hashset is the implementation class of set interface where duplicates are not allowed and insertion order not preserved 
+class HashSetClassConcept.java
+{
+	public static void main(String s[])
+	{
+		HashSet<String> hs=new HashSet<String>();
+		
+		
+	}
+}
